e99ef7387b7e48174a1d3a3cc00af849543033da
Anythingsoup01 <williamenglish1220@gmail.com>
2025-03-02 19:49:57 -0500

    Manually pulled New Ferret files

--- prebuild.lua
+++ prebuild.lua
@@ -1,8 +1,10 @@
 workspace "Ferret"
 
 external "Ferret"
-external "FerretApp"
+external "CatNip"
+external "CatNipApp"
 
 external "vendor/glfw"
 external "vendor/glad"
 external "vendor/imgui"
+external "vendor/ImGuiFileDialog"
